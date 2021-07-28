c9f86dd9856a6b75cc90e05ce827b9d1b0a8bd42
TinhNguyen <LuGia1989@users.noreply.github.com>
2021-07-27 18:51:49 -0700

    Update gpuUtil.py

--- gpu/gpuUtil.py
+++ gpu/gpuUtil.py
@@ -9,14 +9,16 @@ def gpuUtil(f):
         min_gpuUtil = df_gpuUtil['util_in_%'].min()
         max_gpuUtil = df_gpuUtil['util_in_%'].max()
         ave_gpuUtil = int(df_gpuUtil['util_in_%'].mean())
+        std_gpuUtil = float(df_gpuUtil['util_in_%'].std())
         #print(df_encUtil['util_in_%'])
-    return min_gpuUtil, max_gpuUtil, ave_gpuUtil
+    return min_gpuUtil, max_gpuUtil, ave_gpuUtil, std_gpuUtil
 
 def main():
-    min_gpuUtil, max_gpuUtil, ave_gpuUtil = gpuUtil(f)
+    min_gpuUtil, max_gpuUtil, ave_gpuUtil, std_gpuUtil = gpuUtil(f)
     print('Min. GPU Utilization in % = ', min_gpuUtil)
     print('Ave. GPU Utilization in % = ', ave_gpuUtil)
     print('Max. GPU Utilization in % = ', max_gpuUtil)
+    print('Std. GPU Utilization in % = ', '{0:.2f}'.format(std_gpuUtil))
 
 if __name__ == '__main__':
     main()
