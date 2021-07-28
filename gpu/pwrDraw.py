c5b1d2041c0440a8084323f0a3a4d05a0b3e7126
TinhNguyen <LuGia1989@users.noreply.github.com>
2021-07-27 19:00:04 -0700

    Update pwrDraw.py

--- gpu/pwrDraw.py
+++ gpu/pwrDraw.py
@@ -5,18 +5,20 @@ f=sys.argv[1]
 
 def pwrDraw(f):
     with open (f, 'r+') as myfile:
-        df_pwrDraw = pd.read_csv(f, names=["id", "pwrDraw", "timestamp", "util_in_%"])
-        min_pwrDraw = df_pwrDraw['util_in_%'].min()
-        max_pwrDraw = df_pwrDraw['util_in_%'].max()
-        ave_pwrDraw = int(df_pwrDraw['util_in_%'].mean())
+        df_pwrDraw = pd.read_csv(f, names=["id", "pwrDraw", "timestamp", "util_in_watt"])
+        min_pwrDraw = df_pwrDraw['util_in_watt'].min()
+        max_pwrDraw = df_pwrDraw['util_in_watt'].max()
+        ave_pwrDraw = int(df_pwrDraw['util_in_watt'].mean())
+        std_pwrDraw = float(df_pwrDraw['util_in_watt'].std())
         #print(df_pwrDraw['util_in_%'])
-    return min_pwrDraw, max_pwrDraw, ave_pwrDraw
+    return min_pwrDraw, max_pwrDraw, ave_pwrDraw, std_pwrDraw
 
 def main():
-    min_pwrDraw, max_pwrDraw, ave_pwrDraw = pwrDraw(f)
+    min_pwrDraw, max_pwrDraw, ave_pwrDraw, std_pwrDraw = pwrDraw(f)
     print('Min. Power Draw in Watts = ', min_pwrDraw)
     print('Ave. Power Draw in Watts = ', ave_pwrDraw)
     print('Max. Power Draw in Watts = ', max_pwrDraw)
+    print('Std. Power Draw in Watts = ', '{0:.2f}'.format(std_pwrDraw))
 
 if __name__ == '__main__':
     main()
