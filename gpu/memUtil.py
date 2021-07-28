759f0bd0d8d4ba5cf945d593d9ff0c75cab46bb9
TinhNguyen <LuGia1989@users.noreply.github.com>
2021-07-27 18:58:09 -0700

    Update memUtil.py

--- gpu/memUtil.py
+++ gpu/memUtil.py
@@ -9,14 +9,16 @@ def memUtil(f):
         min_memUtil = df_memUtil['util_in_%'].min()
         max_memUtil = df_memUtil['util_in_%'].max()
         ave_memUtil = int(df_memUtil['util_in_%'].mean())
+        std_memUtil = float(df_memUtil['util_in_%'].std())
         #print(df_encUtil['util_in_%'])
-    return min_memUtil, max_memUtil, ave_memUtil
+    return min_memUtil, max_memUtil, ave_memUtil, std_memUtil
 
 def main():
-    min_memUtil, max_memUtil, ave_memUtil = memUtil(f)
+    min_memUtil, max_memUtil, ave_memUtil, std_memUtil = memUtil(f)
     print('Min. Memory Utilization in % = ', min_memUtil)
     print('Ave. Memory Utilization in % = ', ave_memUtil)
     print('Max. Memory Utilization in % = ', max_memUtil)
+    print('Std. Memory Utilization in % = ', '{0:.2f}'.format(std_memUtil))
 
 if __name__ == '__main__':
     main()
